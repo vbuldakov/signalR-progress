98d312bcef42407182d82237c7cb9d07bfe4f7c5
vbuldakov <vbuldakov@gmail.com>
2013-12-03 22:31:56 +0400

    Initial commit

---
+++ RemoteProgress/App_Start/LongTask.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace RemoteProgress.App_Start
+{
+    public class LongTask
+    {
+
+        public static Task DoTaskAsync(string connectionId)
+        {
+            return Task.Factory.StartNew(DoTask, connectionId);
+        }
+
+        private static void DoTask(object state)
+        {
+            string connectionId = state as String;
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<ProgressHub>();
+
+            for (int i = 10; i <= 100; i += 10)
+            {
+                Thread.Sleep(1000);
+
+                hubContext.Clients.Client(connectionId).showProgress(i.ToString());
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ RemoteProgress/App_Start/ProgressHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace RemoteProgress.App_Start
+{
+    public class ProgressHub : Hub
+    {
+        public async void Start()
+        {
+            Clients.Caller.showProgress("Started");
+            await LongTask.DoTaskAsync(Context.ConnectionId);
+            Clients.Caller.onCompleted("Completed");
+        }
+    }
+}
